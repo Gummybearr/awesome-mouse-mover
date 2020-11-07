be43d6266bbe11a5016452d91a368d74de70847f
Gummybearr <ehrudghl123@naver.com>
2020-11-08 02:12:51 +0900

    Remove foreground mode

--- src/awesome-mouse-mover.cpp
+++ src/awesome-mouse-mover.cpp
@@ -2,40 +2,20 @@
 #include <Windows.h>
 #include <conio.h>
 
-char WORKSPACE = 'b';
-
 void showIntro() {
-    //Tells users how to use the program and how they can set the program run in background or foreground.
-    printf("\
-    *********************************************************************** \n \
-        This program moves your mouse to upper left corner every 5 minutes. \n \
-        If you want to execute this program in foreground, press F key.     \n \
-        Otherwise, this program will run in background in 5 seconds.       \n \
-   ***********************************************************************  \n\n");
-}
-
-void getKeyboardInput() {
-    //Stores keyboard inputs and saves to global variable WORKSPACE.
-    int counter = 0;
-    int nKey;
-    while (counter <= 5) {
-        if (_kbhit()) { nKey = _getch();    WORKSPACE = nKey; }
-        ++counter;  Sleep(1000);
-    }
-}
-
-void determineConsoleState() {
-    //Determines process to be run in background or on foreground depending on user input.
-    printf("Program rus ");
-    if (WORKSPACE == 'f' || WORKSPACE == 'F') { 
-        printf("on FOREGROUND\n"); 
-    }
-    else { 
-        printf("in BACKGROUND\n");
-        printf("To terminate the program, you would have to run process manager by pressint ALT + SHIFT + DEL\n");
-        Sleep(2000);
-    }
-    if (!(WORKSPACE == 'f' || WORKSPACE == 'F')) FreeConsole();
+    //Tells users how to use the program and how they can close the program
+    MessageBox(0, 
+        L"\
+    ****************************************************************** \n \
+                                                                       \n \
+          This program moves your mouse every 5 minutes.               \n \
+          This program will run in background.                         \n \
+          If you want to close the program,                            \n \
+          press ALT + SHIFT + DEL to activate process manger.          \n \
+                                                                       \n \
+   ******************************************************************", 
+        L"Awesome-mouse-mover Instruction", 
+        MB_SETFOREGROUND);
 }
 
 void runMacro() {
@@ -43,7 +23,6 @@ void runMacro() {
     int counter = 0;
     while (true) {
         if (counter >= 300) {
-            printf("cursor moves to ( 0,  0 )\n");
             counter = 0;
             SetCursorPos(763, 1060);
             mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
@@ -55,8 +34,7 @@ void runMacro() {
 
 int main(){
     showIntro();
-    getKeyboardInput();
-    determineConsoleState();
+    FreeConsole();
     runMacro();
     return 0;
 }
